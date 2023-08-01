6d49701160ea66b2d55226d3d0094d540a3be2a0
nekhozhin <nekhozhin@netcracker.com>
2023-08-01 09:11:39 +0400

    ads

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -7,38 +7,40 @@ type Props = {
 };
 
 const Layout: FC<PropsWithChildren<Props>> = ({children, ...props}) => (
-    <>
-        <Button
-            as={Link}
-            href="#main"
-            userSelect="none"
-            position="absolute"
-            top="-100px"
-            left="0"
-            _focus={{
-                top: "0"
-            }}
-        >
-            Skip to content
-        </Button>
+        <>
+            <Button
+                as={Link}
+                href="#main"
+                userSelect="none"
+                position="absolute"
+                top="-100px"
+                left="0"
+                _focus={{
+                    top: "0"
+                }}
+            >
+                Skip to content
+            </Button>
 
 
-        <VStack minH="100%" spacing={7}>
-            <Header
-                bgColor={useColorModeValue("lingva.100", "lingva.900")}
-            />
+            <VStack minH="100%" spacing={7}>
+                <Header
+                    bgColor={useColorModeValue("lingva.100", "lingva.900")}
+                />
 
-            <div style={{background: "#e3fbe3", padding: "10px", margin: "5px 10px", "font-size": "12px"}}>
-                Original server <a href="lingva.ml">lingva.ml</a> is down so I've created
-                fallback for me. Sorry for ads but I need 7$/mo to maintain server.
-                Also you can donate me at <a style={{background: "#90ee90", padding: "3px"}}
-                                             href="https://yoomoney.ru/to/41001998657825">DONATE</a>
-            </div>
-            <div style={{"margin-top": "0"}}>
-                <div id="yandex_rtb_R-A-2555887-1" style={{"max-height":"200px"}}>Please allow ad-block to display ads to allow me maintain server for you</div>
-                <script
-                    dangerouslySetInnerHTML={{
-                        __html: `
+                <div style={{background: '#e3fbe3', padding: '10px', margin: '5px 10px', fontSize: '12px'}}>
+                    Original server <a href="lingva.ml">lingva.ml</a> is down so I've created fallback for me. Sorry
+                    for ads but I need 7$/mo to maintain server. Also you can donate me at
+                    <a style={{background: '#90ee90', padding: '3px'}}
+                       href="https://yoomoney.ru/to/41001998657825">DONATE</a>
+                </div>
+                <div style={{marginTop: '0'}}>
+                    <div id="yandex_rtb_R-A-2555887-1" style={{maxHeight: '200px'}}>
+                        Please allow ad-block to display ads to allow me maintain server for you
+                    </div>
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
                             window.yaContextCb=window.yaContextCb||[];
                             
                             const yaScript = document.createElement("script");
@@ -52,26 +54,27 @@ const Layout: FC<PropsWithChildren<Props>> = ({children, ...props}) => (
                                 });
                             });
                         `
-                    }}
-                ></script>
-            </div>
+                        }}
+                    ></script>
+                </div>
 
-            <Flex
-                as="main"
-                id="main"
-                flexGrow={1}
-                w="full"
-                {...props}
-            >
-                {children}
-            </Flex>
+                <Flex
+                    as="main"
+                    id="main"
+                    flexGrow={1}
+                    w="full"
+                    {...props}
+                >
+                    {children}
+                </Flex>
 
-            <Footer
-                bgColor={useColorModeValue("lingva.100", "lingva.900")}
-                color={useColorModeValue("lingva.900", "lingva.100")}
-            />
-        </VStack>
-    </>
-);
+                <Footer
+                    bgColor={useColorModeValue("lingva.100", "lingva.900")}
+                    color={useColorModeValue("lingva.900", "lingva.100")}
+                />
+            </VStack>
+        </>
+    )
+;
 
 export default Layout;
